4c5469712a49716ed57b0443d5af6b0dadd7f72d
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2014-11-28 16:35:29 -0400

    Changed theme to pelican-bootstrap3

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Theme
+THEME="../pelican-themes/pelican-bootstrap3"
